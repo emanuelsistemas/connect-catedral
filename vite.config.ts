62e804581144e045d118272d0d49d70018e64b96
Emanuel Luis <32394604+emanuelsistemas@users.noreply.github.com>
2025-04-15 19:28:26 -0300

    neyf

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
@@ -9,5 +8,9 @@ export default defineConfig({
   },
   server: {
     port: 3000
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true
   }
 });
